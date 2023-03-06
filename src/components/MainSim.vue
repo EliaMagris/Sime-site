b11fda3b1da24f5e8f96fcbd1f0f5be71488a634
Elia Magris <elia.magris@libero.it>
2023-03-06 23:31:25 +0100

    correzioni grafica

--- src/components/MainSim.vue
+++ src/components/MainSim.vue
@@ -3,60 +3,66 @@
     <div class="w-100 photo-bg">
       <div class="d-flex title-container text-center justify-content-center">
         <div class="container-text px-4">
-          <h1 class="text-white title-big">INDACO DECORI <br />azienda di Adriano e Filippo Simeoni</h1>
-          <p class="text-white text-info py-4 fs-4">Più di 30 anni di esperienza</p>
+          <h1 class="text-white title-big">
+            INDACO DECORI <br />azienda di Adriano e Filippo Simeoni
+          </h1>
+          <p class="text-white text-info py-4 fs-4">
+            Più di 30 anni di esperienza
+          </p>
           <router-link to="/about-us">
-          <button type="button" class="btn btn-danger btn-lg">Chiedi un preventivo</button>
+            <button type="button" class="btn btn-danger btn-lg">
+              Chiedi un preventivo
+            </button>
           </router-link>
         </div>
       </div>
     </div>
-    <h2>Chi siamo</h2>
-    <div class="d-flex  justify-content-center">
-      
+    <h2 class="mb-md-2 ChiSiam text-center pt-3 pb-lg-3 pb-xl-2"><i>Chi siamo</i></h2>
+    <div class="d-flex justify-content-center">
       <div>
-        
-      <div class="slideshow">
-    <div class="slideshow-container">
-      <div v-for="(slide, index) in slides" :key="index" class="slide" :class="{'active': index === currentIndex}">
-        <h5>{{ slide.text }}</h5>
+        <div class="slideshow">
+          <div class="slideshow-container">
+            <div
+              v-for="(slide, index) in slides"
+              :key="index"
+              class="slide"
+              :class="{ active: index === currentIndex }"
+            >
+              <h5 class="testoD">{{ slide.text }}</h5>
+            </div>
+          </div>
+          <div class="controls mt-lg-3 mt-xl-2">
+            <button @click="prevSlide" class="prev btn btn-danger">Prev</button>
+            <button @click="nextSlide" class="next btn btn-danger">Next</button>
+          </div>
+        </div>
       </div>
     </div>
-    <div class="controls">
-      <button @click="prevSlide" class="prev btn btn-danger">Prev</button>
-      <button @click="nextSlide" class="next btn btn-danger">Next</button>
-    </div>
-  </div>
-
-        
-        
-      </div>
+    <div>
+      <h2>WEEELAAAAA</h2>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'MainSim',
+  name: "MainSim",
   props: {},
-   data() {
+  data() {
     return {
       slides: [
         {
-          
-          text: "Con la collaborazione di mio figlio Filippo realizziamo con cura e passione ogni intervento affidatoci al fine di garantire ai nostri clienti affidabilità e precisione cercando di soddisfare ogni esigenza portandola alla soluzione più accurata e adatta alle proprie necessità."
+          text: "Con la collaborazione di mio figlio Filippo realizziamo con cura e passione ogni intervento affidatoci al fine di garantire ai nostri clienti affidabilità e precisione cercando di soddisfare ogni esigenza portandola alla soluzione più accurata e adatta alle proprie necessità.",
         },
         {
-          
-          text: "Utilizziamo prodotti di alta qualità all'acqua e atossici per la prevenzione di muffe, problemi di condense e umidità garantendone i risultati nel tempo."
+          text: "Utilizziamo prodotti di alta qualità all'acqua e atossici per la prevenzione di muffe, problemi di condense e umidità garantendone i risultati nel tempo.",
         },
         {
-          
-          text: "Le coibentazioni esterne le realizziamo con materiali innovativi e performanti di ultima generazione, rimaniamo a disposizione per sopralluoghi e formulazione di preventivi gratuiti."
-        }
+          text: "Le coibentazioni esterne le realizziamo con materiali innovativi e performanti di ultima generazione, rimaniamo a disposizione per sopralluoghi e formulazione di preventivi gratuiti.",
+        },
       ],
       currentIndex: 0,
-      intervalId: null
+      intervalId: null,
     };
   },
   mounted() {
@@ -75,14 +81,15 @@ export default {
       this.currentIndex = (this.currentIndex + 1) % this.slides.length;
     },
     prevSlide() {
-      this.currentIndex = (this.currentIndex - 1 + this.slides.length) % this.slides.length;
-    }
+      this.currentIndex =
+        (this.currentIndex - 1 + this.slides.length) % this.slides.length;
+    },
   },
   beforeDestroy() {
     this.stopSlideshow();
-  }
-}
-  </script>
+  },
+};
+</script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
@@ -92,10 +99,17 @@ export default {
   }
 }
 @media screen and (max-width: 991px) {
+  .ChiSiam{
+    margin-bottom: 3rem;
+  };
+  
 }
 @media screen and (max-width: 576px) {
   .title-big {
     font-size: 2rem;
+  }
+  .controls{
+    margin-top: 3rem;
   }
 }
 
@@ -130,7 +144,7 @@ export default {
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  height: 200px;
+  height: 150px;
 }
 .slide {
   display: none;
